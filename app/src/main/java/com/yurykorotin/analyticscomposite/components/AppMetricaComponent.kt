1e8cd3015df010d11ee1d3787e5a7cba326acb4d
Veniamin Borovikov <vnbor08@mail.ru>
2022-09-01 14:43:12 +0400

    Enable app metrica in AppMetricaComponent

--- app/src/main/java/com/yurykorotin/analyticscomposite/components/AppMetricaComponent.kt
+++ app/src/main/java/com/yurykorotin/analyticscomposite/components/AppMetricaComponent.kt
@@ -1,8 +1,8 @@
 package com.yurykorotin.analyticscomposite.components
 
 import android.app.Application
-//import com.yandex.metrica.YandexMetrica
-//import com.yandex.metrica.YandexMetricaConfig
+import com.yandex.metrica.YandexMetrica
+import com.yandex.metrica.YandexMetricaConfig
 import com.yurykorotin.analyticscomposite.events.ACBaseEvent
 import java.io.Serializable
 
@@ -12,14 +12,14 @@ class AppMetricaComponent(
 ) : AnalyticsComponent {
 
     init {
-        /*val config: YandexMetricaConfig = YandexMetricaConfig
+        val config: YandexMetricaConfig = YandexMetricaConfig
             .newConfigBuilder(apiKey)
             .withLogs()
            .build()
 
         YandexMetrica.activate(application.applicationContext, config)
         YandexMetrica.enableActivityAutoTracking(application)
-        YandexMetrica.setLocationTracking(false)*/
+        YandexMetrica.setLocationTracking(false)
     }
 
     override fun trackEvent(acBaseEvent: ACBaseEvent) {
