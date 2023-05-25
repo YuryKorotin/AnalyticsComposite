5512da53807c593c355ea013c766a4512833a0e4
Veniamin Borovikov <vnbor08@mail.ru>
2023-05-25 13:05:27 +0400

    Draft: Setup ECom events

--- app/src/main/java/com/yurykorotin/analyticscomposite/components/appmetrica/ECommerceEvents.kt
+++ app/src/main/java/com/yurykorotin/analyticscomposite/components/appmetrica/ECommerceEvents.kt
@@ -5,5 +5,6 @@ enum class ECommerceEvents {
     BeginCheckoutEvent,
     ShowProductCardEvent,
     ShowScreenEvent,
-    ShowProductDetailsEvent
+    ShowProductDetailsEvent,
+    AddProductEvent
 }
